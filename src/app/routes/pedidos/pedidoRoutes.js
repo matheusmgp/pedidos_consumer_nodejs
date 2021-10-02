296f452bd904249e483ed47fcd0c5009d20c7bc5
matheusmgp <matheus_mgp@hotmail.com>
2021-10-01 22:24:23 -0300

    middleware bearer

--- src/app/routes/pedidos/pedidoRoutes.js
+++ src/app/routes/pedidos/pedidoRoutes.js
@@ -2,11 +2,11 @@ const { celebrate, Segments  } = require('celebrate');
 const express = require('express')
 const PedidoController = require('../../controllers/PedidoController')
 //const validation = require('../../validators/UserValidator')
-//const authMiddleware = require('../../middlewares/auth')
+const authMiddleware = require('../../middlewares/auth')
 
 
 const routes = express.Router();
-//routes.use(authMiddleware);
+routes.use(authMiddleware);
 
 routes.get('/orders/:cnpj', PedidoController.findAllByCnpj);
 
